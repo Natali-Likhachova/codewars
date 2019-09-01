25f3a1279a455322eaa22946afc06505be183c4b
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-09-01 21:15:28 +0300

    added You only need one - Beginner.

---
+++ 8kyu/You only need one - Beginner/index.js
@@ -0,0 +1,22 @@
+/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.*/
+
+/* Link: https://www.codewars.com/kata/57cc975ed542d3148f00015b */
+
+// Long Solution
+function check(a, x) {
+    var coincidence = a.indexOf(x);
+    if (coincidence >= 0) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+// Short Solution
+function check(a,x){
+    return a.includes(x);
+};
