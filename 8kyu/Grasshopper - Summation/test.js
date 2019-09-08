abc91366f18fbab728e4486db39b9fc25477cb18
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-09-08 20:18:22 +0300

    added Grasshopper - Summation.

---
+++ 8kyu/Grasshopper - Summation/test.js
@@ -0,0 +1,7 @@
+/* Sample Tests: */
+describe('summation', function () {
+    it('should return the correct total', function () {
+        Test.assertEquals(summation(1), 1)
+        Test.assertEquals(summation(8), 36)
+    })
+})
