f3b1c22bf273696a686aca5a1f1a3dc585b25872
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-09-28 18:53:28 +0300

    add MakeUpperCase

---
+++ 8kyu/MakeUpperCase/index.js
@@ -0,0 +1,9 @@
+/* Write function makeUpperCase. */
+
+/* Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7 */
+
+// Solution
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
---
+++ 8kyu/MakeUpperCase/test.js
@@ -0,0 +1,6 @@
+/* Sample Tests: */
+describe("Basic Tests", function(){
+    it("should pass the basic tests", function(){
+        Test.assertEquals(makeUpperCase("hello"), "HELLO");
+    });
+});
