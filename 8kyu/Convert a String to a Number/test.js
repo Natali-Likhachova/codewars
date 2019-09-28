8d02d5b63844681e8c71c1724dfbc0da678793fb
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-09-28 18:37:57 +0300

    add Convert a String to a Number

---
+++ 8kyu/Convert a String to a Number/test.js
@@ -0,0 +1,9 @@
+/* Sample Tests: */
+describe( "stringToNumber", function(){
+    it( "should work for the examples" , function(){
+        Test.assertEquals(stringToNumber("1234"),1234)
+        Test.assertEquals(stringToNumber("605"), 605)
+        Test.assertEquals(stringToNumber("1405"),1405)
+        Test.assertEquals(stringToNumber("-7"),  -7)
+    });
+});
