5c98ac8dec6538edfa81db90d2ddd58d52c929f6
Natali <nataliya.likhachova@gmail.com>
2020-08-26 20:25:20 +0300

    added multiply

---
+++ PHP/8kyu/Multiply/index.php
@@ -0,0 +1,10 @@
+<?php
+/* This code does not execute properly. Try to figure out why. */
+
+/* Link: https://www.codewars.com/kata/50654ddff44f800200000004/train/php */
+
+// Long Solution
+function multiply($a, $b) {
+  return $a * $b;
+}
+
