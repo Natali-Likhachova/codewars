eb1f6c9401e1ccf9a8949cbb61194d44de1cea31
Natali <nataliya.likhachova@gmail.com>
2020-08-26 20:30:27 +0300

    added Reversed Strings

---
+++ PHP/8kyu/Reversed Strings/index.php
@@ -0,0 +1,11 @@
+<?php
+/* Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow' */
+
+/* Link: https://www.codewars.com/kata/5168bb5dfe9a00b126000018 */
+
+// Solution
+function solution($str) {
+  return strrev($str);
+}
---
+++ PHP/8kyu/Reversed Strings/test.php
@@ -0,0 +1,9 @@
+<?php
+class ReversedStringsTest extends TestCase {
+  public function testExamples() {
+    $this->assertEquals("dlrow", solution("world"));
+    $this->assertEquals("olleh", solution("hello"));
+    $this->assertEquals("", solution(""));
+    $this->assertEquals('h', solution("h"));
+  }
+}
