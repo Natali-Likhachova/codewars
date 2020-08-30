c297b42ef81290adcbc6fc550ff9303c539e125c
Natali <nataliya.likhachova@gmail.com>
2020-08-30 14:45:52 +0300

    added Number of People in the Bus

---
+++ PHP/7kyu/Number of People in the Bus/test.php
@@ -0,0 +1,12 @@
+<?php
+/* Sample Tests: */
+// PHPUnit Test Examples:
+// TODO: Replace examples and use TDD development by writing your own tests
+class MyTestCases extends TestCase
+{
+  // test function names should start with "test"
+  public function testThatSomethingShouldHappen() {
+    $this->assertEquals("a", "a");
+    $this->assertEquals([0], [0]);
+  }
+}
